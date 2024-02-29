024bda2d279fb92c6b8cbb5df763d320c40a75af
lorenzo <lorenzo.softdev@gmail.com>
2024-02-29 18:01:12 +0100

    init svelte app + tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../styles/app.css';
+</script>
+
+<slot />
+
+<!-- <style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.blue.100);
+	}
+</style> -->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	const name = 'Lorenzo';
+
+	const test = () => {
+		console.log('salut test');
+	};
+</script>
+
+<main class="bg-red-500">
+	<h1 class="text-3xl font-bold underline">
+		Hello world! {name}
+	</h1>
+</main>
